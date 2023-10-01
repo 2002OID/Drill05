9ad862ebae72545de25b67739c5254599ad343b2
2002OID <ykkim308321@gmail.com>
2023-10-01 11:34:25 +0900

    기본 틀 만들기

---
+++ move_for_hand_arraw.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import random
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+
+    frame = (frame + 1) % 8
+
+close_canvas()
