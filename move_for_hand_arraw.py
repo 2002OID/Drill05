a1c9ff69c3fbe7c231013c7c7a78cc328d1a7130
2002OID <ykkim308321@gmail.com>
2023-10-01 11:45:42 +0900

    손가락 생성 및 이동 함수 만들기

--- move_for_hand_arraw.py
+++ move_for_hand_arraw.py
@@ -12,13 +12,21 @@ running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
+arrow_x, arrow_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
+def make_arrow():
+    global x, y, arrow_x, arrow_y
+    if x == arrow_x and y == arrow_y:
+        arrow_x = random.randint(0, TUK_WIDTH + 1)
+        arrow_y = random.randint(0, TUK_HEIGHT + 1)
+    hand_arrow.draw(arrow_x, arrow_y)
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     make_arrow()
-    go_cha_to_arr()
-
+    #go_cha_to_arr()
+    update_canvas()
     frame = (frame + 1) % 8
 
 close_canvas()
