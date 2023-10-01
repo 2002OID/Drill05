1a0287c6903fd056b00504393ac0372268303bf4
2002OID <ykkim308321@gmail.com>
2023-10-01 11:59:21 +0900

    함수 수정
    함수에선 좌표값만 정해주고 그리기는 목두 메인에서

--- move_for_hand_arraw.py
+++ move_for_hand_arraw.py
@@ -13,27 +13,36 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 arrow_x, arrow_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-
+line_frame = 0
+cha_arr = 1
 def make_arrow():
     global x, y, arrow_x, arrow_y
     if x == arrow_x and y == arrow_y:
         arrow_x = random.randint(0, TUK_WIDTH + 1)
         arrow_y = random.randint(0, TUK_HEIGHT + 1)
-    hand_arrow.draw(arrow_x, arrow_y)
+
 def go_cha_to_arrow():
-    global x, y, arrow_x, arrow_y
-    for i in range(0, 50 + 1, 5):
-        t = i / 50
-        x = (1 - t) * x + t * arrow_x
-        y = (1 - t) * y + t * arrow_y
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    global x, y, arrow_x, arrow_y, line_frame
+
+    t = line_frame / 50
+    x = (1 - t) * x + t * arrow_x
+    y = (1 - t) * y + t * arrow_y
+
+
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     make_arrow()
+    hand_arrow.draw(arrow_x, arrow_y)
     go_cha_to_arrow()
+    if x > arrow_x:
+        cha_arr = 0
+    else:
+        cha_arr = 1
+    character.clip_draw(frame * 100, 100 * cha_arr, 100, 100, x, y)
     update_canvas()
+    line_frame = (line_frame + 5) % 50
     frame = (frame + 1) % 8
     delay(0.1)
 
