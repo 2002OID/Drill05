7cc439b23ea8daa3ba113884a00a865a1cf58bc6
2002OID <ykkim308321@gmail.com>
2023-10-01 11:53:03 +0900

    캐릭터가 화살표를 향해 가는 함수

--- move_for_hand_arraw.py
+++ move_for_hand_arraw.py
@@ -20,13 +20,21 @@ def make_arrow():
         arrow_x = random.randint(0, TUK_WIDTH + 1)
         arrow_y = random.randint(0, TUK_HEIGHT + 1)
     hand_arrow.draw(arrow_x, arrow_y)
+def go_cha_to_arrow():
+    global x, y, arrow_x, arrow_y
+    for i in range(0, 50 + 1, 5):
+        t = i / 50
+        x = (1 - t) * x + t * arrow_x
+        y = (1 - t) * y + t * arrow_y
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     make_arrow()
-    #go_cha_to_arr()
+    go_cha_to_arrow()
     update_canvas()
     frame = (frame + 1) % 8
+    delay(0.1)
 
 close_canvas()
